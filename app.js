1a22069ee4a13ec2395be396d63705e8a791e61e
psjfoster <psjfoster@gmail.com>
2023-07-19 09:56:28 +0100

    Add local/session storage use

--- app.js
+++ app.js
@@ -1,112 +1,185 @@
-/* TODO:
- * add notes!
- * create setStat() function for four suits
+/* app.js /
+ * 
+ * consider using events to trigger some of these actions?
  */
 
-// wheel removed
-const deck = [{"suit":"wands","value":1},{"suit":"wands","value":2},{"suit":"wands","value":3},{"suit":"wands","value":4},{"suit":"wands","value":5},{"suit":"wands","value":6},{"suit":"wands","value":7},{"suit":"wands","value":8},{"suit":"wands","value":9},{"suit":"wands","value":10},{"suit":"wands","value":11},{"suit":"wands","value":12},{"suit":"wands","value":13},{"suit":"wands","value":14},{"suit":"cups","value":1},{"suit":"cups","value":2},{"suit":"cups","value":3},{"suit":"cups","value":4},{"suit":"cups","value":5},{"suit":"cups","value":6},{"suit":"cups","value":7},{"suit":"cups","value":8},{"suit":"cups","value":9},{"suit":"cups","value":10},{"suit":"cups","value":11},{"suit":"cups","value":12},{"suit":"cups","value":13},{"suit":"cups","value":14},{"suit":"swords","value":1},{"suit":"swords","value":2},{"suit":"swords","value":3},{"suit":"swords","value":4},{"suit":"swords","value":5},{"suit":"swords","value":6},{"suit":"swords","value":7},{"suit":"swords","value":8},{"suit":"swords","value":9},{"suit":"swords","value":10},{"suit":"swords","value":11},{"suit":"swords","value":12},{"suit":"swords","value":13},{"suit":"swords","value":14},{"suit":"pentacles","value":1},{"suit":"pentacles","value":2},{"suit":"pentacles","value":3},{"suit":"pentacles","value":4},{"suit":"pentacles","value":5},{"suit":"pentacles","value":6},{"suit":"pentacles","value":7},{"suit":"pentacles","value":8},{"suit":"pentacles","value":9},{"suit":"pentacles","value":10},{"suit":"pentacles","value":11},{"suit":"pentacles","value":12},{"suit":"pentacles","value":13},{"suit":"pentacles","value":14},{"suit":"major","number":"0","name":"fool","text":"A player character is punished for a past action."},{"suit":"major","number":"i","name":"magician","text":"The current player gets something they need."},{"suit":"major","number":"ii","name":"high priestess","text":"Something causes the current player to react instinctively."},{"suit":"major","number":"iii","name":"empress","text":"A non-player character wants to help the group."},{"suit":"major","number":"iv","name":"emperor","text":"A non-player character wants to be in charge."},{"suit":"major","number":"v","name":"hierophant","text":"A non-player character wants to play it safe."},{"suit":"major","number":"vi","name":"lovers","text":"The group meets somebody new."},{"suit":"major","number":"vii","name":"chariot","text":"The current situation is brought under control."},{"suit":"major","number":"viii","name":"strength","text":"Something forces the current player to act rashly."},{"suit":"major","number":"ix","name":"hermit","text":"Something affords the current player a moment of calm."},{"suit":"major","number":"xi","name":"justice","text":"Events here have consequences elsewhere."},{"suit":"major","number":"xii","name":"hanged man","text":"The current player must sacrifice something."},{"suit":"major","number":"xiii","name":"death","text":"A non-player character must make a difficult decision."},{"suit":"major","number":"xiv","name":"temperance","text":"The group must progress another way."},{"suit":"major","number":"xv","name":"devil","text":"The current situation begins to deteriorate."},{"suit":"major","number":"xvi","name":"tower","text":"The group's progress thus far is threatened."},{"suit":"major","number":"xvii","name":"star","text":"The current situation requires external influence."},{"suit":"major","number":"xviii","name":"moon","text":"The current player finds something interesting."},{"suit":"major","number":"xix","name":"sun","text":"The group makes good progress."},{"suit":"major","number":"xx","name":"judgement","text":"A non-player character's hard work pays off."},{"suit":"major","number":"xxi","name":"world","text":"A player character is rewarded for a past action."}];
+const debug = sessionStorage.getItem("debug");
+
+const cardsWithoutWheel = [{"suit":"wands","value":1},{"suit":"wands","value":2},{"suit":"wands","value":3},{"suit":"wands","value":4},{"suit":"wands","value":5},{"suit":"wands","value":6},{"suit":"wands","value":7},{"suit":"wands","value":8},{"suit":"wands","value":9},{"suit":"wands","value":10},{"suit":"wands","value":11},{"suit":"wands","value":12},{"suit":"wands","value":13},{"suit":"wands","value":14},{"suit":"cups","value":1},{"suit":"cups","value":2},{"suit":"cups","value":3},{"suit":"cups","value":4},{"suit":"cups","value":5},{"suit":"cups","value":6},{"suit":"cups","value":7},{"suit":"cups","value":8},{"suit":"cups","value":9},{"suit":"cups","value":10},{"suit":"cups","value":11},{"suit":"cups","value":12},{"suit":"cups","value":13},{"suit":"cups","value":14},{"suit":"swords","value":1},{"suit":"swords","value":2},{"suit":"swords","value":3},{"suit":"swords","value":4},{"suit":"swords","value":5},{"suit":"swords","value":6},{"suit":"swords","value":7},{"suit":"swords","value":8},{"suit":"swords","value":9},{"suit":"swords","value":10},{"suit":"swords","value":11},{"suit":"swords","value":12},{"suit":"swords","value":13},{"suit":"swords","value":14},{"suit":"pentacles","value":1},{"suit":"pentacles","value":2},{"suit":"pentacles","value":3},{"suit":"pentacles","value":4},{"suit":"pentacles","value":5},{"suit":"pentacles","value":6},{"suit":"pentacles","value":7},{"suit":"pentacles","value":8},{"suit":"pentacles","value":9},{"suit":"pentacles","value":10},{"suit":"pentacles","value":11},{"suit":"pentacles","value":12},{"suit":"pentacles","value":13},{"suit":"pentacles","value":14},{"suit":"major","number":"0","name":"fool","text":"A player character is punished for a past action."},{"suit":"major","number":"i","name":"magician","text":"The current player gets something they need."},{"suit":"major","number":"ii","name":"high priestess","text":"Something causes the current player to react instinctively."},{"suit":"major","number":"iii","name":"empress","text":"A non-player character wants to help the group."},{"suit":"major","number":"iv","name":"emperor","text":"A non-player character wants to be in charge."},{"suit":"major","number":"v","name":"hierophant","text":"A non-player character wants to play it safe."},{"suit":"major","number":"vi","name":"lovers","text":"The group meets somebody new."},{"suit":"major","number":"vii","name":"chariot","text":"The current situation is brought under control."},{"suit":"major","number":"viii","name":"strength","text":"Something forces the current player to act rashly."},{"suit":"major","number":"ix","name":"hermit","text":"Something affords the current player a moment of calm."},{"suit":"major","number":"xi","name":"justice","text":"Events here have consequences elsewhere."},{"suit":"major","number":"xii","name":"hanged man","text":"The current player must sacrifice something."},{"suit":"major","number":"xiii","name":"death","text":"A non-player character must make a difficult decision."},{"suit":"major","number":"xiv","name":"temperance","text":"The group must progress another way."},{"suit":"major","number":"xv","name":"devil","text":"The current situation begins to deteriorate."},{"suit":"major","number":"xvi","name":"tower","text":"The group's progress thus far is threatened."},{"suit":"major","number":"xvii","name":"star","text":"The current situation requires external influence."},{"suit":"major","number":"xviii","name":"moon","text":"The current player finds something interesting."},{"suit":"major","number":"xix","name":"sun","text":"The group makes good progress."},{"suit":"major","number":"xx","name":"judgement","text":"A non-player character's hard work pays off."},{"suit":"major","number":"xxi","name":"world","text":"A player character is rewarded for a past action."}];
 // wheel to be inserted randomly after halfway
 const wheelCard = {"suit":"major","number":"x","name":"wheel of fortune","text":"Shuffle the deck. Each player gains an experience point at the end of the session."}
+
+// html elements must be explicitly fetched
 const journal = document.getElementById("journal");
-const bars = [
-  document.getElementById("wandBar"),
-  document.getElementById("cupBar"),
-  document.getElementById("swordBar"),
-  document.getElementById("pentacleBar"),
-  document.getElementById("majorBar"),
-];
+
+class Suit {
+  count = 14;
+  draw() {
+    if (this.drawn < this.count) {
+      this.drawn++;
+      if (debug == "true") {
+        this.bar.setAttribute("style", `width: ${(--this.left / 22) * 100}%;`);
+      } else {
+        this.bar.setAttribute("style", `width: ${(--this.left / this.count) * 100}%;`);
+      }
+    }
+  }
+  drawn = 0;
+  left = this.count;
+  loadScore(l) {
+    this.score = l;
+    switch (l) {
+      case 1:
+        this.stat.setAttribute("style", "font-style: italic;");
+        this.em.innerHTML = "&minus;&minus;";
+        break;
+      case 2:
+        this.stat.setAttribute("style", ";");
+        this.em.innerHTML = "";
+        break;
+      case 3:
+        this.stat.setAttribute("style", "font-weight: bold;");
+        this.em.innerHTML = "&plus;&plus;";
+        break;
+    }
+  }
+  reset() {
+    this.drawn = 0;
+    this.left = this.count;
+    this.bar.setAttribute("style", "width: 100%;");
+  }
+  set() {
+    this.drawn = 0;
+    this.bar.setAttribute("style", `width: ${(this.left / 22) * 100}%;`);
+  }
+  setScore() {
+    this.score++;
+    if (this.score > 3) {
+      this.score = 1;
+      this.stat.setAttribute("style", "font-style: italic;");
+      this.em.innerHTML = "&minus;&minus;";
+    } else {
+      if (this.score == 3) {
+        this.stat.setAttribute("style", "font-weight: bold;");
+        this.em.innerHTML = "&plus;&plus;";
+      } else {
+        this.stat.setAttribute("style", ";");
+        this.em.innerHTML = "";
+      }
+    }
+    saveState();
+  }
+  score = 2;
+}
+
+const suits = [ new Suit(), new Suit(), new Suit(), new Suit(), new Suit() ];
+// wands
+suits[0].bar = document.getElementById("wandBar");
+suits[0].stat = document.getElementsByTagName("wands")[0];
+suits[0].em = document.getElementsByTagName("wands")[0].getElementsByTagName("stat")[0];
+// cups
+suits[1].bar = document.getElementById("cupBar");
+suits[1].stat = document.getElementsByTagName("cups")[0];
+suits[1].em = document.getElementsByTagName("cups")[0].getElementsByTagName("stat")[0];
+// swords
+suits[2].bar = document.getElementById("swordBar");
+suits[2].stat = document.getElementsByTagName("swords")[0];
+suits[2].em = document.getElementsByTagName("swords")[0].getElementsByTagName("stat")[0];
+// pentacles
+suits[3].bar = document.getElementById("pentacleBar");
+suits[3].stat = document.getElementsByTagName("pentacles")[0];
+suits[3].em = document.getElementsByTagName("pentacles")[0].getElementsByTagName("stat")[0];
+// major
+suits[4].count = 22;
+suits[4].bar = document.getElementById("majorBar");
+
+loadState();
+
+var xpThisSession = 0;
+
+function loadState() {
+  if (localStorage.getItem("state0")) {
+    suits[0].loadScore(Number(localStorage.getItem("state0")));
+    suits[1].loadScore(Number(localStorage.getItem("state1")));
+    suits[2].loadScore(Number(localStorage.getItem("state2")));
+    suits[3].loadScore(Number(localStorage.getItem("state3")));
+  }
+  if (sessionStorage.getItem("xp")) {
+    const xp = document.getElementById("xp").getElementsByTagName("em")[0];
+    xp.setAttribute("style", "font-weight: bold;");
+    xpThisSession = Number(sessionStorage.getItem("xp"));
+    xp.innerText = `${xpThisSession}`;
+  }
+}
+
+function saveState() {
+  localStorage.setItem("state0", suits[0].score);
+  localStorage.setItem("state1", suits[1].score);
+  localStorage.setItem("state2", suits[2].score);
+  localStorage.setItem("state3", suits[3].score);
+}
 
 var cardsDrawn = 0;
 var pleaseShuffle = false;
-var xpThisSession = 0;
-
-var wandStat = 3, cupStat = 3, swordStat = 3, pentacleStat = 3;
-var wandsLeft = 14, cupsLeft = 14, swordsLeft = 14, pentaclesLeft = 14, majorLeft = 22;
-
-var cards;
+var deck;
 shuffle();
 
 function shuffle() {
-  cards = deck.slice();
+  const drawbutton = document.getElementById("drawbutton");
+  const shufflebutton = document.getElementById("shufflebutton");
+  deck = cardsWithoutWheel.slice();
   var i = 77, j, k;
   while(--i > 0) {
     j = Math.floor(Math.random() * (i + 1));
-    k = cards[j];
-    cards[j] = cards[i];
-    cards[i] = k;
-  }  
+    k = deck[j];
+    deck[j] = deck[i];
+    deck[i] = k;
+  }
   var w = Math.ceil(Math.random() * 39) + 39;
-  cards.splice(w, 0, wheelCard);
-  // console.log(`Wheel is card ${w}.`);
+  deck.splice(w, 0, wheelCard);
+  if (debug == "true") {
+    console.log(`Wheel is card ${w + 1}.`);
+  }
   cardsDrawn = 0;
-  resetBars();
+  shufflebutton.setAttribute("disabled", "disabled");
+  drawbutton.removeAttribute("disabled");
   pleaseShuffle = false;
-}
-
-function resetBars() {
-  bars.forEach(b => {
-    b.setAttribute("style", "width: 100%;");
-  });  
-  wandsLeft = 14; cupsLeft = 14; swordsLeft = 14; pentaclesLeft = 14; majorLeft = 22;
+  if (debug == "true") {
+    var i = 0, w = 0, c = 0, s = 0, p = 0, m = 1;
+    while (deck[i].name != "wheel of fortune") {
+      if (deck[i].suit == "wands") {
+        w += 1;
+      }
+      if (deck[i].suit == "cups") {
+        c += 1;
+      }
+      if (deck[i].suit == "swords") {
+        s += 1;
+      }
+      if (deck[i].suit == "pentacles") {
+        p += 1;
+      }
+      if (deck[i].suit == "major") {
+        m += 1;
+      }
+      i++;
+    }
+    suits[0].left = w; suits[0].set();
+    suits[1].left = c; suits[1].set();
+    suits[2].left = s; suits[2].set();
+    suits[3].left = p; suits[3].set();
+    suits[4].left = m; suits[4].set();
+    console.log(`Wands: ${w}\nCups: ${c}\nSwords: ${s}\nPentacles: ${p}\nMajor: ${m}`);
+  } else {
+    suits.forEach(s => { s.reset(); });
+  }
 }
 
 function draw() {
   if (pleaseShuffle) {
   } else {
-    getCardText(cards[cardsDrawn++]);
+    getCardText(deck[cardsDrawn++]);
   }
-}
-
-function drawWand() {
-  bars[0].setAttribute("style", `width: ${(--wandsLeft / 14) * 100}%;`);
-}  
-function drawCup() {
-  bars[1].setAttribute("style", `width: ${(--cupsLeft / 14) * 100}%;`);
-}  
-function drawSword() {
-  bars[2].setAttribute("style", `width: ${(--swordsLeft / 14) * 100}%;`);
-}  
-function drawPentacle() {
-  bars[3].setAttribute("style", `width: ${(--pentaclesLeft / 14) * 100}%;`);
-}
-function drawMajor() {
-  bars[4].setAttribute("style", `width: ${(--majorLeft / 22) * 100}%;`);
-}
-
-function wands() {
-  if (wandStat == 3) {
-    wandStat = 1;
-  } else {
-    wandStat++;
-  }  
-  setStat("wands", wandStat);
-}
-
-function cups() {
-  if (cupStat == 3) {
-    cupStat = 1;
-  } else {
-    cupStat++;
-  }  
-  setStat("cups", cupStat);
-}
-
-function swords() {
-  if (swordStat == 3) {
-    swordStat = 1;
-  } else {
-    swordStat++;
-  }  
-  setStat("swords", swordStat);
-}
-
-function pentacles() {
-  if (pentacleStat == 3) {
-    pentacleStat = 1;
-  } else {
-    pentacleStat++;
-  }    
-  setStat("pentacles", pentacleStat);
 }
 
 function setStat(suit, statScore) {
@@ -133,20 +206,20 @@ function getCardText(card) {
     printResult(`You draw the ${card.value} of ${card.suit}.`);
     switch (card.suit) {
       case "wands":
-        drawWand();
-        statCheck(wandStat);
+        suits[0].draw();
+        statCheck(suits[0].score);
         break;
       case "cups":
-        drawCup();
-        statCheck(cupStat);
+        suits[1].draw();
+        statCheck(suits[1].score);
         break;
       case "swords":
-        drawSword();
-        statCheck(swordStat);
+        suits[2].draw();
+        statCheck(suits[2].score);
         break;
       case "pentacles":
-        drawPentacle();
-        statCheck(pentacleStat);
+        suits[3].draw();
+        statCheck(suits[3].score);
         break;
     }
   } else {
@@ -184,7 +257,7 @@ function getCardTextMajor(card) {
     result += `${card.name}.`;
     printResult(result);
     printPrompt(`${card.text}`);
-    drawMajor();
+    suits[4].draw();
   } else {
     printResult(`You draw the wheel of fortune! `);
     spinWheel();
@@ -209,11 +282,17 @@ function printPrompt(prompt) {
 }
 
 function spinWheel() {
+  const drawbutton = document.getElementById("drawbutton");
   const print = document.createElement("p");
-  pleaseShuffle = true;
+  const shufflebutton = document.getElementById("shufflebutton");
   const xp = document.getElementById("xp").getElementsByTagName("em")[0];
+  suits[4].draw();
+  pleaseShuffle = true;
+  shufflebutton.removeAttribute("disabled");
+  drawbutton.setAttribute("disabled", "disabled");
   xp.setAttribute("style", "font-weight: bold;");
   xp.innerText = `${xpThisSession += 1}`;
+  sessionStorage.setItem("xp", xpThisSession);
   print.setAttribute("style", "font-weight: bold;");
   print.innerText = "Shuffle the deck.\nEach player gains an experience point at the end of the session.\n"
   journal.append(print);
